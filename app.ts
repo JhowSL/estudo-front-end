51078769ec4d4ea7da9cd50f4b262fd2e18fadf3
JhowSL <jon.1lima@hotmail.com>
2023-11-09 15:39:46 -0300

    obejetos e interfaces

--- app.ts
+++ app.ts
@@ -1,6 +1,26 @@
-const soma = (a: number, b: number) => {
-    console.log(a + b);
+interface Pessoa {
+    nome: string;
+    idade: number;
+    profissao?: string;
 }
 
-soma(1, 2);
-soma(10, 11);
+const pessoa: Pessoa = {
+    nome: 'Jhow',
+    idade: 20,
+    profissao: 'Programador'
+}
+
+const pessoa2: Pessoa = {
+    nome: 'Jhow Jr.',
+    idade: 20,
+}
+
+const pessoas: Array<Pessoa> = [
+    pessoa,
+    pessoa2
+]
+const numeros: number[] = [1, 2, 3, 4, 5];
+
+console.log(pessoas[0].nome); // Isto irá imprimir o primeiro elemento do array 'pessoas'
+console.log(pessoas[1].nome); // Isto irá imprimir o segundo elemento do array 'pessoas'
+console.log(numeros[3])    // Isto irá imprimir o quarto elemento do array 'numeros'
