badbab6e3bf17ffe8c32ee4a0edf1825df526212
JhowSL <jon.1lima@hotmail.com>
2023-11-13 01:54:21 -0300

    feat: conteudo de manioulçao de array

--- app.ts
+++ app.ts
@@ -1,97 +1,17 @@
 // Array de Strings
 const myArray: string[] = ["apple", "banana", "orange"];
-console.log(myArray);
-console.log(myArray[0]);
-console.log(myArray.length);
+console.log('manipulando array');
+console.log('==================');
+const buscarElemento = myArray.find(elemento => elemento === 'banana');
+console.log('buscando elemento com find: ', buscarElemento);
 console.log('------------------');
-console.log('Array de Strings');
+myArray.forEach(myArray => console.log(myArray));
+myArray.forEach(myArray => {
+    if (myArray === 'banana') {
+        console.log('banana encontrada');
+    }
+});
+console.log('buscando elemento com forEach');
 console.log('------------------');
-console.log('array original: ', myArray);
-console.log('------------------');
-console.log('array adicionando elemento: ', myArray.push('grape'));
-console.log('------------------');
-console.log('array com elemento adicionado: ', myArray);
-console.log('------------------');
-console.log('array removendo elemento: ', myArray.pop());
-console.log('------------------');
-console.log('array com elemento removido: ', myArray);
-console.log('------------------');
-
-// Array de números
-const numberArray: number[] = [1, 2, 3, 4, 5];
-console.log(numberArray);
-console.log(numberArray[1]);
-console.log(myArray.length);
-console.log('------------------');
-console.log('Array de números');
-console.log('------------------');
-console.log('array original: ', numberArray);
-console.log('------------------');
-console.log('array adicionando elemento: ', numberArray.push(6));
-console.log('------------------');
-console.log('array com elemento adicionado: ', numberArray);
-console.log('------------------');
-console.log('array removendo elemento: ', numberArray.pop());
-console.log('------------------');
-console.log('array com elemento removido: ', numberArray);
-console.log('------------------');
-
-// Array de booleanos
-const booleanArray: boolean[] = [true, false, true];
-console.log(booleanArray);
-console.log(booleanArray[2]);
-console.log(myArray.length);
-console.log('------------------');
-console.log('Array de booleanos');
-console.log('------------------');
-console.log('array original: ', booleanArray);
-console.log('------------------');
-console.log('array adicionando elemento: ', booleanArray.push(false));
-console.log('------------------');
-console.log('array com elemento adicionado: ', booleanArray);
-console.log('------------------');
-console.log('array removendo elemento: ', booleanArray.pop());
-console.log('------------------');
-console.log('array com elemento removido: ', booleanArray);
-console.log('------------------');
-
-// Array de objetos
-const objectArray: { name: string, age: number }[] = [
-    { name: "Alice", age: 25 },
-    { name: "Bob", age: 30 },
-    { name: "Charlie", age: 35 }
-];
-console.log(objectArray);
-console.log(objectArray[1]);
-console.log(myArray.length);
-console.log('------------------');
-console.log('Array de objetos');
-console.log('------------------');
-console.log('array original: ', objectArray);
-console.log('------------------');
-console.log('array adicionando elemento: ', objectArray.push({ name: "Dan", age: 40 }));
-console.log('------------------');
-console.log('array com elemento adicionado: ', objectArray);
-console.log('------------------');
-console.log('array removendo elemento: ', objectArray.pop());
-console.log('------------------');
-console.log('array com elemento removido: ', objectArray);
-console.log('------------------');
-
-// Array de qualquer tipo
-const anyArray: any[] = [1, "apple", true, { name: "Alice", age: 25 }];
-console.log(anyArray);
-console.log(myArray.length);
-console.log('------------------');
-console.log('Array de qualquer tipo');
-console.log('------------------');
-console.log('array original: ', anyArray);
-console.log('------------------');
-console.log('array adicionando elemento: ', anyArray.push('grape'));
-console.log('------------------');
-console.log('array com elemento adicionado: ', anyArray);
-console.log('------------------');
-console.log('array removendo elemento: ', anyArray.pop());
-console.log('------------------');
-console.log('array com elemento removido: ', anyArray);
-console.log('------------------');
+myArray.map(myArray => console.log(myArray));
+console.log('buscando elemento com map');
