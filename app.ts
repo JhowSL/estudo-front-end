11dab1668ebd150c5b07228cd35ddf7ead099e81
JhowSL <jon.1lima@hotmail.com>
2023-11-13 15:14:29 -0300

    conteudo de heranças e classes

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
-class Account {
+// Herança
+abstract class Account {
     name: string;
     accountNumber: number;
+    balance: number = 0;
 
     constructor(name: string, accountNumber: number) {
         this.name = name;
@@ -16,8 +18,32 @@ class Account {
     }
 }
 
-const newAccount = new Account("Jhow", 1);
-console.log(newAccount);
+class PeopleAccount extends Account {
+    doc_id: number;
+    constructor(doc_id: number, name: string, accountNumber: number) {
+        super(name, accountNumber);
+        this.doc_id = doc_id;
+    }
+}
 
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Jhow', 10);
+console.log(peopleAccount);
+
+// Classes
+/* class Admin extends Account {
+
+
+    constructor(name: string, accountNumber: number) {
+        super(name, accountNumber);
+
+    }
+
+    getbalance = () => {
+        console.log("Seu saldo é de: " + this.balance);
+    }
+}
+
+const adminAccount: Admin = new Admin('Jhow', 1);
+console.log(adminAccount);
 const newAccount2 = new Account("Jonh", 2);
-newAccount2.deposit();
+console.log(newAccount2); */
