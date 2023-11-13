0659bbae93e2cbc6d3958362826682e550818835
JhowSL <jon.1lima@hotmail.com>
2023-11-13 01:27:45 -0300

    conteudo de array

--- app.ts
+++ app.ts
@@ -1,40 +1,77 @@
-//If e Else
-/* const num: number = 15;
-if (num > 15) {
-    console.log('Number is greater than 15');
-} else if (num === 15) {
-    console.log('Number is 15');
-} else {
-    console.log('Number is less than 15');
-} */
+// Array de Strings
+const myArray: string[] = ["apple", "banana", "orange"];
+console.log(myArray);
+console.log(myArray[0]);
+console.log(myArray.length);
+console.log('------------------');
+console.log('array original: ', myArray);
+console.log('------------------');
+console.log('array adicionando elemento: ', myArray.push('grape'));
+console.log('------------------');
+console.log('array removendo elemento: ', myArray.pop());
+console.log('------------------');
+console.log('array com elemento removido: ', myArray);
+console.log('------------------');
 
-//Obejto Literal
-/* const typeUser = {
-    admin: 'Você é um administrador',
-    student: 'Você é um estudante',
-    viewer: 'Você é um espectador'
-}
+// Array de números
+const numberArray: number[] = [1, 2, 3, 4, 5];
+console.log(numberArray);
+console.log(numberArray[1]);
+console.log(myArray.length);
+console.log('------------------');
+console.log('array original: ', numberArray);
+console.log('------------------');
+console.log('array adicionando elemento: ', numberArray.push(6));
+console.log('------------------');
+console.log('array removendo elemento: ', numberArray.pop());
+console.log('------------------');
+console.log('array com elemento removido: ', numberArray);
+console.log('------------------');
 
-function valideUser(user: string) {
-    console.log(typeUser[user as keyof typeof typeUser]);
-}
+// Array de booleanos
+const booleanArray: boolean[] = [true, false, true];
+console.log(booleanArray);
+console.log(booleanArray[2]);
+console.log(myArray.length);
+console.log('------------------');
+console.log('array original: ', booleanArray);
+console.log('------------------');
+console.log('array adicionando elemento: ', booleanArray.push(false));
+console.log('------------------');
+console.log('array removendo elemento: ', booleanArray.pop());
+console.log('------------------');
+console.log('array com elemento removido: ', booleanArray);
+console.log('------------------');
 
-const user = 'admin';
+// Array de objetos
+const objectArray: { name: string, age: number }[] = [
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 30 },
+    { name: "Charlie", age: 35 }
+];
+console.log(objectArray);
+console.log(objectArray[1]);
+console.log(myArray.length);
+console.log('------------------');
+console.log('array original: ', objectArray);
+console.log('------------------');
+console.log('array adicionando elemento: ', objectArray.push({ name: "Dan", age: 40 }));
+console.log('------------------');
+console.log('array removendo elemento: ', objectArray.pop());
+console.log('------------------');
+console.log('array com elemento removido: ', objectArray);
+console.log('------------------');
 
-valideUser(user);
-valideUser('student');
-valideUser('viewer'); */
-
-//For
-/* for (let i = 0; i < 5; i++) {
-    console.log(i);
-
-}
- */
-
-//While
-/* let n = 2;7
-while (n < 6) {
-    console.log(n);
-    n++;
-} */
+// Array de qualquer tipo
+const anyArray: any[] = [1, "apple", true, { name: "Alice", age: 25 }];
+console.log(anyArray);
+console.log(myArray.length);
+console.log('------------------');
+console.log('array original: ', anyArray);
+console.log('------------------');
+console.log('array adicionando elemento: ', anyArray.push('grape'));
+console.log('------------------');
+console.log('array removendo elemento: ', anyArray.pop());
+console.log('------------------');
+console.log('array com elemento removido: ', anyArray);
+console.log('------------------');
