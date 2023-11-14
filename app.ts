1073fed6a74c545fb3e5aff282b605a5e91ece49
JhowSL <jon.1lima@hotmail.com>
2023-11-14 12:24:33 -0300

    feat: visibiliade

--- app.ts
+++ app.ts
@@ -11,13 +11,8 @@ import { PeopleAccount } from './class/PeopleAccount';
 // O construtor da classe PeopleAccount é chamado com os argumentos: 1 (número da conta), 'Jhow' (nome do titular) e 10 (saldo inicial)
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Jhow', 10);
 
-// Chama o método deposit da instância peopleAccount, que provavelmente adiciona um valor ao saldo da conta
+// Imprime a instância da conta de pessoa física no console
+console.log(peopleAccount);
+
+// Chama o método de depósito na conta de pessoa física
 peopleAccount.deposit();
-
-// Cria uma nova instância da classe CompanyAccount, representando uma conta de empresa
-// O construtor da classe CompanyAccount é chamado com os argumentos: 'Black Bank' (nome da empresa) e 11 (número da conta)
-const companyAccount: CompanyAccount = new CompanyAccount('Black Bank', 11);
-
-// Chama o método deposit da instância companyAccount, que provavelmente adiciona um valor ao saldo da conta
-// O resultado é então impresso no console
-console.log(companyAccount.deposit());
--- class/BankAccount.ts
+++ class/BankAccount.ts
@@ -1,18 +1,36 @@
 // Classe abstrata BankAccount que representa uma conta bancária genérica
 export abstract class BankAccount {
-    name: string; // Nome do titular da conta
+    private name: string; // Nome do titular da conta
     accountNumber: number; // Número da conta
     balance: number = 0; // Saldo da conta, inicializado como 0
+    private status: boolean = true; // Status da conta, inicializado como true
 
     // Construtor da classe
+    // Recebe o nome do titular e o número da conta como parâmetros
     constructor(name: string, accountNumber: number) {
         this.name = name; // Define o nome do titular da conta
         this.accountNumber = accountNumber; // Define o número da conta
     }
+    // Método para definir o nome do titular da conta
+    // Recebe o novo nome como parâmetro
+    // Não retorna nada
+    setName = (name: string): void => {
+        this.name = name; // Define o novo nome do titular da conta
+        console.log('Nome alterado com sucesso'); // Imprime uma mensagem de sucesso no console
+    }
+
+    // Método para obter o nome do titular da conta
+    // Retorna o nome do titular da conta como uma string
+    getName = (): string => {
+        return this.name;
+    }
+
 
     // Método para depositar dinheiro na conta
     deposit = () => {
-        console.log("Você depositou dinheiro na sua conta");
+        if (this.validateStatus()) {
+            console.log("Você depositou dinheiro na sua conta");
+        }
     }
 
     // Método para sacar dinheiro da conta
@@ -23,5 +41,15 @@ export abstract class BankAccount {
     // Método para obter o saldo da conta
     getBalance = () => {
         console.log(this.balance);
+    }
+
+    // Método privado para validar o status da conta
+    // Este método retorna o status da conta se a conta estiver ativa
+    // Se a conta estiver inativa, ele lança um erro
+    private validateStatus = (): boolean => {
+        if (this.status) {
+            return this.status;
+        }
+        throw new Error('Conta inativa');
     }
 }
